58f171de9f6d07bf3461694f828a2f62649c2a97
Victor Berchet <victor@suumit.com>
2025-03-27 08:55:04 +0100

    add env var

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li>
+            TEST_EV = {process.env.TEST_EV}
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
